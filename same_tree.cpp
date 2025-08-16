e608e6dc582439a7e6ce653ad93a72f0ecb9e578
Asif Hossain <asifiscoding@gmail.com>
2025-08-17 04:59:46 +0600

    same_tree.cpp

---
+++ same_tree.cpp
@@ -0,0 +1,17 @@
+//  OJ          : Leetcode
+//  Problem Name: Same Tree
+//  Problem Link: https://leetcode.com/problems/same-tree/
+    
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if( p==NULL or q == NULL){
+            return (p == q);
+        }
+
+        return (p->val == q->val)
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
+    }
+};
